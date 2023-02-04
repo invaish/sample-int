6d1acc802bd134cc2b30cc2441ef3d3e7715276d
invaish <124432918+invaish@users.noreply.github.com>
2023-02-04 17:19:49 +0530

    Add files via upload

---
+++ Twowheeler.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Twowheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int noOfWheels = 4;
+		System.out.println("noOfWheels = " +noOfWheels);
+		short noOfMirrors = 2;
+		System.out.println("noOfMirrors = " +noOfMirrors);
+		long chassisNumber = 987654321l;
+			System.out.println("chassisNumber = " +chassisNumber);
+			
+			boolean isPunctured= false;	
+			System.out.println("bikeisPunctured = " +isPunctured);
+			
+			double runningKM = 2234.5678;
+			System.out.println("runningKM = " +runningKM);
+			
+			String bikeName = "Activa";
+					System.out.println("Activa");
+
+	}
+
+}
