e15ed2703f0b720541b4350cec514d66c31b10c9
invaish <124432918+invaish@users.noreply.github.com>
2023-02-20 19:15:15 +0530

    Add files via upload

---
+++ org/AxisBank.java
@@ -0,0 +1,14 @@
+package week3.day1;
+
+public class AxisBank extends BankInfo {
+	
+	/*overriding */
+	
+	public void deposit() {
+		System.out.println("Amount 40,000");
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ org/BankInfo.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class BankInfo {
+	
+	public void saving() {
+		System.out.println("Amount 50,000");
+
+	}
+	public void fixed() {
+		System.out.println("12% interest rate");
+
+	}
+	public void deposit() {
+		System.out.println("Amount 30,000");
+
+	}
+
+}
